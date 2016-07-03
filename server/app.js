475e761fc930bb6aca9260accb912761286bc0f4
Polina Soshnin <psoshnin@gmail.com>
2016-07-03 14:19:57 -0700

    setup basic express server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var port = process.env.PORT || 5000;
+// var middleware = require('./config/middleware');
+
+// middleware(app, express);
+
+server.listen(port, function() {
+  console.log('Listening on port', port);
+});
